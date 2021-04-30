80ebe8cd51704a8029a655d185c5c3dfc98a51e0
yutaro910 <yayantsukebo@gmail.com>
2021-04-30 18:02:16 +0900

    カード情報のトークン化と送付、マイページへカード情報の取得と表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
+  
   def show
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    card = Card.find_by(user_id: current_user.id)
+
+    redirect_to new_card_path and return unless card.present?
+    # unless card.present?
+      # redirect_to new_card_path and return
+    # end と同義
+    
+    customer = Payjp::Customer.retrieve(card.customer_token)
+    # 先程のカード情報を元に、顧客情報を取得
+    @card = customer.cards.first
+    # 顧客が複数カード登録している場合、その内の「最初のカード（first）情報を取得する」
   end
 
   def updeta
