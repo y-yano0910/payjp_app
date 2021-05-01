3be8844b888d043bab89821fa5df2a0a28a3d73f
yutaro910 <yayantsukebo@gmail.com>
2021-05-01 22:49:39 +0900

    商品購入機能と売り切れ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,33 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: :order
+
   def index
     @items = Item.all
+  end
+
+  def order
+    redirect_to new_card_path and return unless current_user.card.present?
+
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    customer_token = current_user.card.customer_token
+    Payjp::Charge.create(
+      amount: @item.price,
+      customer: customer_token,
+      currency: 'jpy'
+    )
+    # 支払い機能を利用するための環境変数の読み込み
+    # カード情報の受取のためのPAYJPサイドに送るトークン定義
+    # 支払い情報の生成
+
+    ItemOrder.create(item_id: params[:id])
+    # 商品のid情報を「item_id」として保存する
+
+    redirect_to root_path
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 end
